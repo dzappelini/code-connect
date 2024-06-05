0889e28c8ebb75e6b2142c18bbd2752d9b3718a0
Douglas Zappelini Neto <61122920+dzappelini@users.noreply.github.com>
2024-06-05 02:40:43 +0000

    adicionado simulador json-server / funcoes para leer e imprimir JSON nos cards

--- code-connect/src/app/page.js
+++ code-connect/src/app/page.js
@@ -1,25 +1,29 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// src/app/page.js
 import { CardPost } from "@/components/CardPost";
 
-const post = {
-  id: 1,
-  cover:
-    "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-  title: "Introdução ao React",
-  slug: "introducao-ao-react",
-  body: "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-  markdown:
-    "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-  author: {
-    id: 101,
-    name: "Ana Beatriz",
-    username: "anabeatriz_dev",
-    avatar:
-      "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png",
-  },
-};
+async function getAllPosts() {
+  const response = await fetch("http://localhost:3042/posts");
 
-export default function Home() {
-  return <CardPost post={post} />;
+  if (!response.ok) {
+    console.log("ERRO");
+    return [];
+  }
+
+  return response.json();
+}
+
+export default async function Home() {
+  const posts = await getAllPosts();
+
+  if (posts.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main>
+      {posts.map((post) => (
+        <CardPost key={post.id} post={post} />
+      ))}
+    </main>
+  );
 }
--- code-connect/src/components/CardPost/index.jsx
+++ code-connect/src/components/CardPost/index.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import { Avatar } from "../Avatar";
+import styles from "./cardpost.module.css";
 
 export const CardPost = ({ post }) => {
   return (
-    <article>
+    <article className={styles.cardpost}>
       <header>
         <Image
           src={post.cover}
